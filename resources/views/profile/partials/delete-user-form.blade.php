435640c57c82a27bebec5b3e3938dbf92592e19f
nhat191024 <richberchannel01@gmail.com>
2025-03-31 15:38:06 +0700

    chore: refactor delete user form for improved structure and consistency

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -9,13 +9,10 @@
         </p>
     </header>
 
-    <x-danger-button
-        x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    <x-button.danger-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">{{ __('Delete Account') }}</x-button.danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-        <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
+        <form class="p-6" method="post" action="{{ route('profile.destroy') }}">
             @csrf
             @method('delete')
 
@@ -28,27 +25,21 @@
             </p>
 
             <div class="mt-6">
-                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+                <x-input-label class="sr-only" value="{{ __('Password') }}" for="password" />
 
-                <x-text-input
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="mt-1 block w-3/4"
-                    placeholder="{{ __('Password') }}"
-                />
+                <x-text-input id="password" class="mt-1 block w-3/4" name="password" type="password" placeholder="{{ __('Password') }}" />
 
-                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+                <x-input-error class="mt-2" :messages="$errors->userDeletion->get('password')" />
             </div>
 
             <div class="mt-6 flex justify-end">
-                <x-secondary-button x-on:click="$dispatch('close')">
+                <x-button.secondary-button x-on:click="$dispatch('close')">
                     {{ __('Cancel') }}
-                </x-secondary-button>
+                </x-button.secondary-button>
 
-                <x-danger-button class="ms-3">
+                <x-button.danger-button class="ms-3">
                     {{ __('Delete Account') }}
-                </x-danger-button>
+                </x-button.danger-button>
             </div>
         </form>
     </x-modal>
