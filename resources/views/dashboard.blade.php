c4c8aedb9095237fd662ff56350b583fce2c6d34
nhat191024 <richberchannel01@gmail.com>
2025-03-31 21:49:08 +0700

    style: reorder class attributes for improved readability in dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-xs dark:bg-gray-800 sm:rounded-lg">
+            <div class="shadow-xs overflow-hidden bg-white sm:rounded-lg dark:bg-gray-800">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
