c417e07113cf4f3cacc3bf40e33573bfdeda42e5
nhat191024 <richberchannel01@gmail.com>
2025-03-31 22:19:40 +0700

    refactor: merge attributes into dropdown component for improved flexibility

--- resources/views/components/actions/dropdown.blade.php
+++ resources/views/components/actions/dropdown.blade.php
@@ -13,7 +13,7 @@
     };
 @endphp
 
-<div class="dropdown {{ $alignmentClasses }}">
+<div {{ $attributes->merge(['class' => 'dropdown ' . $alignmentClasses]) }}>
     <div class="m-1 flex items-center" tabindex="0" role="button"> {{ $trigger }}</div>
     <ul class="dropdown-content menu bg-base-100 rounded-box z-1 {{ $width }} border p-2 shadow-sm dark:border-gray-700 dark:bg-gray-800" tabindex="0">
         {{ $content }}
