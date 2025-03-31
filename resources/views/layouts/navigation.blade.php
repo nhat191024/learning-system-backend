66c80737fb2607a21fe744f1e557b2fb2b252de8
nhat191024 <richberchannel01@gmail.com>
2025-03-31 22:19:35 +0700

    refactor: replace button with drawer component in navigation layout for improved structure

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,11 +1,7 @@
 <nav class="border border-gray-100 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800">
     <div class="navbar mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="navbar-start">
-            <div class="btn btn-ghost btn-circle" tabindex="0" role="button">
-                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
-                </svg>
-            </div>
+            <x-drawer />
         </div>
         <div class="navbar-center">
             <div class="flex shrink-0 items-center">
@@ -17,17 +13,14 @@
         <div class="navbar-end">
             <x-utils.dark-mode-toggle />
 
-            <x-actions.dropdown align="right" width="48">
+            <x-actions.dropdown class="ms-2" align="right" width="52">
                 <x-slot name="trigger">
-                    <button class="focus:outline-hidden inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-black transition duration-150 ease-in-out hover:text-gray-700 dark:bg-gray-800 dark:text-white dark:hover:text-gray-300">
-                        <div>{{ Auth::user()->name }}</div>
-
-                        <div class="ms-1">
-                            <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                        </div>
-                    </button>
+                    <div>{{ Auth::user()->name }}</div>
+                    <div class="ms-1">
+                        <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
                 </x-slot>
                 <x-slot name="content">
                     <x-actions.dropdown-link :href="route('profile.edit')">
