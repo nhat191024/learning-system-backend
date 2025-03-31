874cdaf289485357ff20d0b29eb85a74f25d0399
nhat191024 <richberchannel01@gmail.com>
2025-03-31 12:10:44 +0700

    feat: add user management routes for admin panel

---
+++ routes/admin/user.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/users')->name('users.')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('index');
+    Route::get('/create', [UserController::class, 'create'])->name('create');
+    Route::post('/store', [UserController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [UserController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [UserController::class, 'update'])->name('update');
+    Route::get('/status/{id}', [UserController::class, 'status'])->name('status');
+});
