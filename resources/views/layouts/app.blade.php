8f0aacf4ef9c00adc4b41c8372fe8861b89c736a
nhat191024 <richberchannel01@gmail.com>
2025-03-31 12:12:44 +0700

    style: adjust header layout for improved responsiveness

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,7 +59,7 @@
         <!-- Page Heading -->
         @isset($header)
             <header class="bg-white shadow dark:bg-gray-800">
-                <div class="mx-auto max-w-7xl md:flex md:justify-between md:items-center px-4 py-6 sm:px-6 lg:px-8">
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
                     {{ $header }}
                 </div>
             </header>
