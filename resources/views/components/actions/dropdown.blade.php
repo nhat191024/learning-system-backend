b55644e3efa9c96ccc832a5e4badb8744b109ea5
nhat191024 <richberchannel01@gmail.com>
2025-03-31 20:58:41 +0700

    feat: update dropdown component to display on small screens

--- resources/views/components/actions/dropdown.blade.php
+++ resources/views/components/actions/dropdown.blade.php
@@ -13,7 +13,7 @@
     };
 @endphp
 
-<div class="relative hidden sm:flex" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+<div class="relative hidden sm:block" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
     <div @click="open = ! open">
         {{ $trigger }}
     </div>
