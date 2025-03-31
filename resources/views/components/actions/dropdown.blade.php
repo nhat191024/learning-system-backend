8294c7420635be8afa40fe698a0d7c28bd850d15
nhat191024 <richberchannel01@gmail.com>
2025-03-31 22:14:36 +0700

    refactor: update dropdown component structure and styling for improved usability

--- resources/views/components/actions/dropdown.blade.php
+++ resources/views/components/actions/dropdown.blade.php
@@ -1,27 +1,21 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700'])
+@props(['align' => 'right', 'width' => '52'])
 
 @php
     $alignmentClasses = match ($align) {
-        'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
-        'top' => 'origin-top',
-        default => 'ltr:origin-top-right rtl:origin-top-left end-0',
+        'left' => 'dropdown-start',
+        'center' => 'dropdown-center',
+        default => 'dropdown-end',
     };
 
     $width = match ($width) {
-        '48' => 'w-48',
+        '52' => 'w-52',
         default => $width,
     };
 @endphp
 
-<div class="relative hidden sm:block" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
-    <div @click="open = ! open">
-        {{ $trigger }}
-    </div>
-
-    <div class="{{ $width }} {{ $alignmentClasses }} absolute z-50 mt-2 rounded-md shadow-lg" x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
-        x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" style="display: none;" @click="open = false">
-        <div class="{{ $contentClasses }} rounded-md ring-1 ring-black ring-opacity-5">
-            {{ $content }}
-        </div>
-    </div>
+<div class="dropdown {{ $alignmentClasses }}">
+    <div class="m-1 flex items-center" tabindex="0" role="button"> {{ $trigger }}</div>
+    <ul class="dropdown-content menu bg-base-100 rounded-box z-1 {{ $width }} border p-2 shadow-sm dark:border-gray-700 dark:bg-gray-800" tabindex="0">
+        {{ $content }}
+    </ul>
 </div>
