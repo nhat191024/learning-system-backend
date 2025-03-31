4fa5740442f447082fdef216f29fa97dd880a4af
nhat191024 <richberchannel01@gmail.com>
2025-03-31 14:48:18 +0700

    chore: refactor layout classes in admin class index view for consistency

--- resources/views/admin/class/index.blade.php
+++ resources/views/admin/class/index.blade.php
@@ -8,8 +8,8 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="mx-auto max-w-full sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-xs dark:bg-gray-800 sm:rounded-lg">
+        <div class="max-w-full mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
                 </div>
