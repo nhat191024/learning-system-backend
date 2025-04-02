0b220df6876bfa0c65a7d76bafd49811cf703422
nhat191024 <richberchannel01@gmail.com>
2025-04-02 12:15:55 +0700

    feat: add detail, edit, update, and destroy routes for class management

--- routes/admin/class.php
+++ routes/admin/class.php
@@ -7,14 +7,14 @@ Route::prefix('class')->name('class.')->group(function () {
     Route::get('/template', [ClassController::class, 'template'])->name('template');
     Route::get('/', [ClassController::class, 'index'])->name('index');
     Route::post('/store', [ClassController::class, 'store'])->name('store');
+    Route::get('/detail/{id}', [ClassController::class, 'detail'])->name('detail');
+    Route::get('/edit/{id}', [ClassController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [ClassController::class, 'update'])->name('update');
+    Route::get('/destroy/{id}', [ClassController::class, 'destroy'])->name('destroy');
 
-    Route::get('/hide-class/{class_id}', [ClassController::class, 'hideClass'])->name('hideClass');
-    Route::get('/edit-class/{class_id}', [ClassController::class, 'editClass'])->name('editClass');
-    Route::put('/update-class/{class_id}', [ClassController::class, 'updateClass'])->name('updateClass');
-    Route::get('/{id}', [ClassController::class, 'show'])->name('show');
-    Route::get('/{id}/assignment/{assignment_id}', [ClassController::class, 'show'])->name('assignmentDetails');
-    Route::get('/assignment/{assignment_id}/details', [ClassController::class, 'assignmentDetailsJson'])->name('assignmentDetailsJson');
-    Route::post('/{id}/toggle-status', [ClassController::class, 'toggleClassStatus'])->name('toggleStatus');
-    Route::get('/{id}/export', [ClassController::class, 'export'])->name('export');
-    Route::post('/{class}/import-confirm', [ClassController::class, 'importConfirm'])->name('importConfirm');
+    // Route::get('/{id}/assignment/{assignment_id}', [ClassController::class, 'show'])->name('assignmentDetails');
+    // Route::get('/assignment/{assignment_id}/details', [ClassController::class, 'assignmentDetailsJson'])->name('assignmentDetailsJson');
+    // Route::post('/{id}/toggle-status', [ClassController::class, 'toggleClassStatus'])->name('toggleStatus');
+    // Route::get('/{id}/export', [ClassController::class, 'export'])->name('export');
+    // Route::post('/{class}/import-confirm', [ClassController::class, 'importConfirm'])->name('importConfirm');
 });
