bd16219eb15a4e8360e05310f37228e22b37caa6
nhat191024 <richberchannel01@gmail.com>
2025-04-02 15:19:52 +0700

    feat: refactor user management controller to improve user creation, updating, and status management

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,148 +2,133 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use App\Models\User;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    /**
+     * Display a listing of the user.
+     */
     public function index()
     {
-        $users = User::all();
-        $roles = [
-            1 => 'Quản trị',
-            2 => 'Giảng viên',
-            3 => 'Sinh viên',
-            4 => 'giám sát viên',
-        ];
-        return view('users.index', compact('users', 'roles'));
+        $users = User::all()->load('role');
+        return view('admin.user.index', compact('users'));
     }
 
+    /**
+     * Show the form for creating a new user.
+     */
     public function create()
     {
-        return view('users.create');
+        $roles = Role::all();
+        return view('admin.user.create', compact('roles'));
     }
 
-    public function store(Request $request)
+    /**
+     * Store a newly created user in storage.
+     */
+    public function store(StoreUserRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Avatar phải là hình ảnh
-            'password' => 'required|string|min:8|confirmed',
-            'gender' => 'required|in:male,female,other',
-            'role_id' => 'required|in:1,2,3,4',
-            'status' => 'required|boolean',
-        ], [
-            'name.required' => 'Tên người dùng là bắt buộc.',
-            'email.required' => 'Email là bắt buộc.',
-            'email.email' => 'Email phải là một địa chỉ email hợp lệ.',
-            'email.unique' => 'Email này đã được sử dụng.',
-            'avatar.image' => 'Avatar phải là tệp hình ảnh.',
-            'avatar.mimes' => 'Avatar phải có định dạng jpeg, png, jpg, hoặc gif.',
-            'avatar.max' => 'Avatar không được vượt quá 2MB.',
-            'password.required' => 'Mật khẩu là bắt buộc.',
-            'password.min' => 'Mật khẩu phải có ít nhất 8 ký tự.',
-            'password.confirmed' => 'Mật khẩu xác nhận không khớp.',
-            'gender.required' => 'Vui lòng chọn giới tính.',
-            'gender.in' => 'Giới tính không hợp lệ.',
-            'role_id.required' => 'Vui lòng chọn vai trò người dùng.',
-            'role_id.in' => 'Vai trò không hợp lệ.',
-            'status.required' => 'Trạng thái là bắt buộc.',
-            'status.boolean' => 'Trạng thái phải là giá trị đúng hoặc sai.',
-        ]);
+        DB::beginTransaction();
 
-        if ($validator->fails()) {
-            return redirect()->back()
-                ->withErrors($validator)
-                ->withInput();
+        try {
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'avatar' => 'upload/avt/default.png',
+                'username' => $request->username,
+                'password' => Hash::make('default_password'),
+                'gender' => $request->gender,
+                'role_id' => $request->role_id,
+                'status' => $request->status,
+            ]);
+
+            DB::commit();
+
+            return redirect()->route('admin.users.index')->with('success', 'User has been created successfully!');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'An error occurred while creating the user. Please try again! error: ' . $e->getMessage())->withInput();
         }
-        $avatarPath = null;
-        if ($request->hasFile('avatar')) {
-            $avatarPath = $request->file('avatar')->store('avatars', 'public');
-        }
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'avatar' => $avatarPath,
-            'username' => $request->username,
-            'password' => Hash::make($request->password),
-            'gender' => $request->gender,
-            'role_id' => $request->role_id,
-        ]);
-
-        return redirect()->route('users.index')->with('success', 'Người dùng đã được tạo thành công!');
     }
 
-    public function show(string $id)
-    {
-        //
-    }
 
+    /**
+     * Show the form for editing the specified user.
+     */
     public function edit(string $id)
     {
-        $users = User::findOrFail($id);
-        return view('users.edit', compact('users'));
+        $user = User::findOrFail($id);
+        $roles = Role::all();
+        return view('admin.user.edit', compact('user', 'roles'));
     }
 
-    public function update(Request $request, string $id)
+    /**
+     * Update the specified user in storage.
+     */
+    public function update(UpdateUserRequest $request, string $id)
     {
-        $users = User::findOrFail($id);
+        DB::beginTransaction();
 
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email|unique:users,email,' . $id,
-            'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'name' => 'required|string|max:255',
-            'gender' => 'required|in:male,female,other',
-            'role_id' => 'required|in:1,2,3,4',
-        ],[
-            'email.required' => 'Email là bắt buộc.',
-            'email.email' => 'Email không hợp lệ.',
-            'email.unique' => 'Email này đã được sử dụng.',
-            'avatar.image' => 'Avatar phải là tệp hình ảnh.',
-            'avatar.mimes' => 'Avatar chỉ chấp nhận các định dạng jpeg, png, jpg, gif.',
-            'avatar.max' => 'Avatar không được vượt quá 2MB.',
-            'name.required' => 'Tên người dùng là bắt buộc.',
-            'gender.required' => 'Giới tính là bắt buộc.',
-            'gender.in' => 'Giới tính không hợp lệ.',
-            'role_id.required' => 'Vui lòng chọn vai trò.',
-            'role_id.in' => 'Vai trò không hợp lệ.',
-        ]);
+        try {
+            $users = User::findOrFail($id);
 
-        if ($validator->fails()) {
-            return redirect()->route('users.edit', ['id' => $users->id])
-                ->withErrors($validator)
-                ->withInput();
+            $avatarPath = $users->avatar;
+            if ($request->hasFile('avatar')) {
+                $image = $request->file('avatar');
+                $imageName = time() . '_' . $image->getClientOriginalName();
+                $image->move(public_path('upload/avt'), $imageName);
+                $avatarPath = 'upload/avt/' . $imageName;
+            }
+
+            $users->update([
+                'email' => $request->email,
+                'name' => $request->name,
+                'gender' => $request->gender,
+                'role_id' => $request->role_id,
+                'avatar' => $avatarPath,
+            ]);
+
+            DB::commit();
+
+            return redirect()->route('admin.users.index')->with('success', 'User information has been updated successfully!');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'An error occurred while updating user information. Please try again! error: ' . $e->getMessage())->withInput();
         }
-        if ($request->hasFile('avatar')) {
-            $avatarPath = $request->file('avatar')->store('avatars', 'public');
-            $users->avatar = $avatarPath;
+    }
+
+    /**
+     * Remove the specified user from storage.
+     */
+    public function destroy($id)
+    {
+        DB::beginTransaction();
+
+        try {
+            if (Auth::user()->id == $id) {
+                DB::rollBack();
+                return redirect()->back()->with('error', 'You cannot change status your own account!');
+            }
+
+            $users = User::findOrFail($id);
+            $users->status = !$users->status;
+            $users->save();
+
+            DB::commit();
+
+            return redirect()->back()->with('success', 'User status has been updated successfully!');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'An error occurred while updating user status. Please try again! error: ' . $e->getMessage());
         }
-
-        $users->update([
-            'email' => $request->input('email'),
-        'name' => $request->input('name'),
-        'gender' => $request->input('gender'),
-        'role_id' => $request->input('role_id'),
-        ]);
-
-        return redirect()->route('users.index')->with('success', 'Thông tin người dùng đã được cập nhật thành công!');
-    }
-
-    public function status($id)
-    {
-        $users = User::findOrFail($id);
-        $users->status = !$users->status;
-        $users->save();
-
-        return redirect()->route('users.index')->with('success', 'Trạng thái người dùng đã được cập nhật!');
-    }
-
-    public function destroy(string $id)
-    {
-        //
     }
 }
