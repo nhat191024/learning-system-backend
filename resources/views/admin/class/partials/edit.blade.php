49fe186e75e370babe40a6c14fedb913110fc581
nhat191024 <richberchannel01@gmail.com>
2025-04-02 13:57:56 +0700

    feat: add edit modal for class management with form fields for code, name, categories, description, and teacher

---
+++ resources/views/admin/class/partials/edit.blade.php
@@ -0,0 +1,55 @@
+<x-actions.modal class="border border-gray-300 dark:border-gray-700" :id="'createClass'">
+    <form action="{{ route('admin.class.update', $class->id) }}" method="POST">
+        @csrf
+
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('Edit class') }} {{ $class->name }}
+        </h2>
+
+        <div class="mt-6">
+            <x-inputs.input-label value="{{ __('Code') }}" for="code" />
+            <x-inputs.text-input id="code" class="mt-2 w-full" name="code" type="text" required :value="old('code', $class->code)" autofocus placeholder="{{ __('Class code') }}" />
+            <x-inputs.input-error class="mt-2" :messages="$errors->get('code')" />
+        </div>
+
+        <div class="mt-6">
+            <x-inputs.input-label value="{{ __('Name') }}" for="name" />
+            <x-inputs.text-input id="name" class="mt-2 w-full" name="name" type="text" required :value="old('name', $class->name)" autofocus placeholder="{{ __('Class name') }}" />
+            <x-inputs.input-error class="mt-2" :messages="$errors->get('name')" />
+        </div>
+
+        <div class="mt-6">
+            <x-inputs.input-label value="{{ __('Categories') }}" for="categories" />
+            <div class="mt-1"></div>
+            <x-inputs.select-input id="categories" class="select-search-modal mt-2 w-full" name="categories[]" required multiple>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+            </x-inputs.select-input>
+            <x-inputs.input-error class="mt-2" :messages="$errors->get('categories')" />
+        </div>
+
+        <div class="mt-6">
+            <x-inputs.input-label value="{{ __('Description') }}" for="description" />
+            <x-inputs.text-input id="description" class="mt-2 w-full" name="description" type="text" required :value="old('description')" autofocus placeholder="{{ __('Class description') }}" />
+            <x-inputs.input-error class="mt-2" :messages="$errors->get('description')" />
+        </div>
+
+        <div class="mt-6">
+            <x-inputs.input-label value="{{ __('Teacher') }}" for="teacher" />
+            <div class="mt-1"></div>
+            <x-inputs.select-input id="teacher" class="select-search-modal mt-2 w-full" name="teacher_id" required>
+                @foreach ($teachers as $teacher)
+                    <option value="{{ $teacher->id }}">{{ $teacher->name }}</option>
+                @endforeach
+            </x-inputs.select-input>
+            <x-inputs.input-error class="mt-2" :messages="$errors->get('teacher_id')" />
+        </div>
+
+        <div class="modal-action">
+            <x-buttons.success class="ms-3">
+                {{ __('Xác nhận') }}
+            </x-buttons.success>
+        </div>
+    </form>
+</x-actions.modal>
