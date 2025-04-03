75a0e89234393c743b2b8fc497bb6442f118b33e
nhat191024 <richberchannel01@gmail.com>
2025-04-03 21:22:07 +0700

    feat: add relationship for certificates in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,4 +85,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(CourseEnrollment::class, 'student_id');
     }
+
+    public function certificates()
+    {
+        return $this->hasMany(Certificate::class, 'student_id');
+    }
 }
