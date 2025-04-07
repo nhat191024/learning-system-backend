3e4db54ee1a69fe2a54f5bb90a27bc92948e9d98
nhat191024 <richberchannel01@gmail.com>
2025-04-07 17:58:03 +0700

    refactor: update student routes for improved clarity and consistency

--- routes/admin/student.php
+++ routes/admin/student.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/student')->name('student.')->group(function () {
-    Route::post('/store', [StudentController::class, 'store'])->name('store');
-
-    Route::delete('/remove-student/{class_id}/{student_id}', [StudentController::class, 'removeStudentFromAClass'])->name('removeStudent');
+    Route::post('/store/{id}', [StudentController::class, 'store'])->name('store');
+    Route::get('/destroy/{class_id}/{student_id}', [StudentController::class, 'destroy'])->name('destroy');
+    Route::get('/certificate/{class_id}/{student_id}', [StudentController::class, 'certificate'])->name('certificate');
+    Route::get('/certificate/delete/{class_id}/{student_id}', [StudentController::class, 'destroyCertificate'])->name('destroyCertificate');
 });
