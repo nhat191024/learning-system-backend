aade9f14a0ff65eb2e6158c4583aa9036303698c
nhat191024 <richberchannel01@gmail.com>
2025-04-19 15:18:33 +0700

    feat: add status column to course_assignments table for better assignment management

--- database/migrations/2024_12_26_160316_create_course_assignments_table.php
+++ database/migrations/2024_12_26_160316_create_course_assignments_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->string('duration')->nullable();
+            $table->enum('status', ['published', 'closed']);
             $table->timestamps();
         });
     }
