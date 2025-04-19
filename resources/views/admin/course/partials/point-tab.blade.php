c873f126272f4537cb088d17e6e99ed3c2d0ad6b
nhat191024 <richberchannel01@gmail.com>
2025-04-19 15:19:23 +0700

    feat: add point tab for displaying assignment scores and total points

---
+++ resources/views/admin/course/partials/point-tab.blade.php
@@ -0,0 +1,24 @@
+<div id="tab4" class="mt-2 hidden overflow-hidden bg-white opacity-0 shadow-sm transition-all duration-300 ease-in-out sm:rounded-lg dark:bg-gray-800">
+    <div class="p-6 text-gray-900 dark:text-gray-100">
+        <x-datatables>
+            <x-slot name="header">
+                <th>{{ __('No.') }}</th>
+                <th>{{ __('Assignment') }}</th>
+                <th>{{ __('Name') }}</th>
+                <th>{{ __('Point') }}</th>
+                <th>{{ __('Total point') }}</th>
+            </x-slot>
+            @foreach ($assignmentNames as $key => $name)
+                @foreach ($assignmentPoints as $secondKey => $point)
+                    <tr>
+                        <td>1</td>
+                        <td>{{ $name }}</td>
+                        <td>{{ $point['name'] }}</td>
+                        <td>{{ $point['points'][$key]['score'] }}</td>
+                        <td>{{ $point['totalScore'][$key]['totalScore'] }}</td>
+                    </tr>
+                @endforeach
+            @endforeach
+        </x-datatables>
+    </div>
+</div>
