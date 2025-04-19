5ad31fa7596f74b07df7d98ccde197e5caf160d8
nhat191024 <richberchannel01@gmail.com>
2025-04-19 08:41:47 +0700

    fix: correct route casing for store method in class assignment routes

--- routes/admin/class_assignment.php
+++ routes/admin/class_assignment.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('class-assignment')->name('classAssignment.')->group(function () {
     Route::get('/detail/{id}/{classId}', [ClassAssignmentController::class, 'detail'])->name('detail');
+    route::post('/store/{classId}', [ClassAssignmentController::class, 'store'])->name('store');
     Route::get('/destroy/{id}', [ClassAssignmentController::class, 'destroy'])->name('destroy');
 });
