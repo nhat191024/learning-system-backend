c148af7a43c900964e851456e9628351a904aee5
nhat191024 <richberchannel01@gmail.com>
2025-04-19 07:18:58 +0700

    feat: enhance quiz bank functionality with question management and dynamic choice handling

--- app/Http/Controllers/QuizBankController.php
+++ app/Http/Controllers/QuizBankController.php
@@ -3,58 +3,125 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\AssignmentQuiz;
-use App\Models\Category;
-use App\Models\Choice;
-use App\Models\Quiz;
-use App\Models\QuizPackage;
-use App\Models\QuizPackageCategory;
-use App\Models\User;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreQuestionRequest;
+
 use Maatwebsite\Excel\Facades\Excel;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Quiz;
+use App\Models\User;
+use App\Models\Choice;
+use App\Models\Category;
+use App\Models\QuizPackage;
+use App\Models\AssignmentQuiz;
+use App\Models\QuizPackageCategory;
 
 class QuizBankController extends Controller
 {
     public function index()
     {
-        if (Auth::check()) {
-            $quizBank = QuizPackage::orderBy('created_at', 'DESC')->get();
-            $categories = Category::orderBy('created_at', 'DESC')->get();
-            $quizzes = Quiz::orderBy('created_at', 'DESC')->get();
-            $profile = User::findOrFail(Auth::id());
-            return view('QuizBank.index', compact('quizBank', 'categories', 'quizzes', 'profile'));
-        } else {
-            return redirect()->route('admin.login');
+        $quizBank = QuizPackage::orderBy('created_at', 'DESC')->with('creator', 'categories')->get();
+        $categories = Category::orderBy('created_at', 'DESC')->get();
+        $quizzes = Quiz::orderBy('created_at', 'DESC')->get();
+        $profile = User::findOrFail(Auth::id());
+        return view('admin.quiz_bank.index', compact('quizBank', 'categories', 'quizzes', 'profile'));
+    }
+
+    public function questions($id)
+    {
+        $quizBankId = $id;
+        $quizBank = QuizPackage::with('quizzes.choices')->findOrFail($id);
+        $questions = $quizBank->quizzes->map(function ($quiz) {
+            return [
+                'id' => $quiz->id,
+                'question' => $quiz->question,
+                "choices" => $quiz->choices->map(function ($choice) {
+                    return [
+                        'id' => $choice->id,
+                        'choice' => $choice->choice,
+                        'isCorrect' => $choice->is_correct,
+                    ];
+                }),
+            ];
+        });
+        return view('admin.quiz_bank.question', compact('questions', 'quizBankId'));
+    }
+
+    public function questionsStore(StoreQuestionRequest $request, $id)
+    {
+        DB::beginTransaction();
+        try {
+            foreach ($request->questions as $question) {
+                $quiz = Quiz::create([
+                    'question' => $question['question'],
+                    'quiz_package_id' => $id,
+                ]);
+
+                foreach ($question['choices'] as $index => $choice) {
+                    $isCorrect = $index == $question['correct_answer'] ? 1 : 0;
+                    $quiz->choices()->create([
+                        'choice' => $choice,
+                        'is_correct' => $isCorrect,
+                    ]);
+                }
+            }
+            DB::commit();
+            return redirect()->back()->with('success', 'Added questions successfully!');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'Error: ' . $th->getMessage());
         }
     }
 
+    public function questionDestroy($id)
+    {
+        DB::beginTransaction();
+        try {
+            $quiz = Quiz::findOrFail($id);
+            if ($quiz) {
+                $quiz->delete();
+                $choices = Choice::where('quiz_id', $quiz->id)->get();
+                if ($choices) {
+                    foreach ($choices as $choice) {
+                        $choice->delete();
+                    }
+                }
+                DB::commit();
+                return redirect()->back()->with('success', 'Deleted question successfully!');
+            }
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'Failed to delete question!');
+        }
+    }
+
+
     public function createQuizBank(Request $request)
     {
-        if (Auth::check()) {
-            try {
-                $createQuizBank = QuizPackage::create([
-                    'creator_id' => Auth::id(),
-                    'title' => $request->quiz_name,
-                    'description' => $request->quiz_description,
-                    'quiz_id_range' => $request->quiz_id_range,
-                    'status' => 'published',
-                    'type' => $request->type,
-                ]);
+        dd($request->all());
+        try {
+            $createQuizBank = QuizPackage::create([
+                'creator_id' => Auth::id(),
+                'title' => $request->quiz_name,
+                'description' => $request->quiz_description,
+                'quiz_id_range' => $request->quiz_id_range,
+                'status' => 'published',
+                'type' => $request->type,
+            ]);
 
-                foreach ($request->categories as $category) {
-                    QuizPackageCategory::insert([
-                        'quiz_package_id' => $createQuizBank->id,
-                        'category_id' => $category,
-                    ]);
-                }
-                return redirect()->route('quiz-bank.index')->with('success', 'Tạo kho quiz thành công!');
-            } catch (\Throwable $th) {
-                return redirect()->route('quiz-bank.index')->with('error', 'Tạo kho quiz thất bại!');
+            foreach ($request->categories as $category) {
+                QuizPackageCategory::insert([
+                    'quiz_package_id' => $createQuizBank->id,
+                    'category_id' => $category,
+                ]);
             }
-        } else {
-            return redirect()->route('admin.login');
+            return redirect()->route('quiz-bank.index')->with('success', 'Tạo kho quiz thành công!');
+        } catch (\Throwable $th) {
+            return redirect()->route('quiz-bank.index')->with('error', 'Tạo kho quiz thất bại!');
         }
     }
 
@@ -125,246 +192,5 @@ class QuizBankController extends Controller
         } else {
             return redirect()->route('admin.login');
         }
-    }
-
-    public function addQuestion(Request $request)
-    {
-        if (Auth::check()) {
-            try {
-                $request->validate([
-                    'question_name' => 'required|string|max:255',
-                    'anwser_name_1' => 'max:255',
-                    'anwser_name_2' => 'max:255',
-                    'anwser_name_3' => 'max:255',
-                    'anwser_name_4' => 'max:255',
-                    'anwser' => 'required|in:1,2,3,4',
-                ]);
-
-                $count = 0;
-
-                if ($request->anwser_name_1 != null) $count++;
-                if ($request->anwser_name_2 != null) $count++;
-                if ($request->anwser_name_3 != null) $count++;
-                if ($request->anwser_name_4 != null) $count++;
-
-                if ($count < $request->anwser) {
-                    return redirect()->route('quiz-bank.index')->with('error', 'Vui lòng chọn câu trả lời đúng!');
-                }
-
-                $checkQuestion = Quiz::where('question', $request->question_name);
-                if($checkQuestion) {
-                    return redirect()->route('quiz-bank.index')->with('error', 'Câu hỏi đã tồn tại!');
-                }
-
-                $addQuestion = Quiz::create([
-                    'question' => $request->question_name,
-                    'quiz_package_id' => $request->quiz_package_id,
-                ]);
-
-                $anwsers = [
-                    [
-                        'choice' => $request->input('anwser_name_1'),
-                        'quiz_id' => $addQuestion->id,
-                        'is_correct' => $request->input('anwser') == 1 ? 1 : 0,
-                    ],
-                    [
-                        'choice' => $request->input('anwser_name_2'),
-                        'quiz_id' => $addQuestion->id,
-                        'is_correct' => $request->input('anwser') == 2 ? 1 : 0,
-                    ],
-                    [
-                        'choice' => $request->input('anwser_name_3'),
-                        'quiz_id' => $addQuestion->id,
-                        'is_correct' => $request->input('anwser') == 3 ? 1 : 0,
-                    ],
-                    [
-                        'choice' => $request->input('anwser_name_4'),
-                        'quiz_id' => $addQuestion->id,
-                        'is_correct' => $request->input('anwser') == 4 ? 1 : 0,
-                    ],
-                ];
-
-                $anwsers = array_filter($anwsers, function ($anwser) {
-                    return !empty($anwser['choice']);
-                });
-
-                if (empty($anwsers)) {
-                    return redirect()->route('quiz-bank.index')->with('error', 'Vui lòng nhập ít nhất một câu trả lời.');
-                }
-
-                foreach ($anwsers as $anwser) {
-                    $addQuestion->choices()->create($anwser);
-                }
-
-                return redirect()->route('quiz-bank.index')->with('success', 'Tạo câu hỏi thành công!');
-            } catch (\Throwable $th) {
-                return redirect()->route('quiz-bank.index')->with('error', 'Tạo câu hỏi thất bại!')->with('message', $th->getMessage());
-            }
-        } else {
-            return redirect()->route('admin.login');
-        }
-    }
-
-    public function addQuestionWithExcel(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'file' => 'required|mimes:xlsx,xls|max:2048',
-        ]);
-
-        if ($validator->fails()) {
-            return back()->withErrors($validator)->withInput();
-        }
-
-        $file = $request->file('file');
-        $path = $file->getRealPath();
-
-        try {
-            $data = Excel::toArray([], $file);
-
-            if (!empty($data)) {
-                foreach ($data[0] as $index => $row) {
-                    if ($index === 0) {
-                        continue;
-                    }
-                    $questionText = $row[0] ?? null;
-                    $answer1 = $row[1] ?? null;
-                    $isCorrect1 = strtolower($row[2] ?? '') === 'Đúng' ? 1 : 0;
-                    $answer2 = $row[3] ?? null;
-                    $isCorrect2 = strtolower($row[4] ?? '') === 'Đúng' ? 1 : 0;
-                    $answer3 = $row[5] ?? null;
-                    $isCorrect3 = strtolower($row[6] ?? '') === 'Đúng' ? 1 : 0;
-                    $answer4 = $row[7] ?? null;
-                    $isCorrect4 = strtolower($row[8] ?? '') === 'Đúng' ? 1 : 0;
-
-                    if (!$questionText) {
-                        continue; 
-                    }
-
-                    $existingQuestion = Quiz::where('question', $questionText)->exists();
-                    if ($existingQuestion) {
-                        continue;
-                    }
-
-                    $newQuestion = Quiz::create([
-                        'question' => $questionText,
-                        'quiz_package_id' => $request->quiz_package_id,
-                    ]);
-
-                    $answers = [
-                        [
-                            'choice' => $answer1,
-                            'quiz_id' => $newQuestion->id,
-                            'is_correct' => $isCorrect1,
-                        ],
-                        [
-                            'choice' => $answer2,
-                            'quiz_id' => $newQuestion->id,
-                            'is_correct' => $isCorrect2,
-                        ],
-                        [
-                            'choice' => $answer3,
-                            'quiz_id' => $newQuestion->id,
-                            'is_correct' => $isCorrect3,
-                        ],
-                        [
-                            'choice' => $answer4,
-                            'quiz_id' => $newQuestion->id,
-                            'is_correct' => $isCorrect4,
-                        ],
-                    ];
-
-                    $answers = array_filter($answers, function ($answer) {
-                        return !empty($answer['choice']);
-                    });
-
-                    foreach ($answers as $answer) {
-                        $newQuestion->choices()->create($answer);
-                    }
-                }
-
-                return redirect()->route('quiz-bank.index')->with('success', 'Dữ liệu đã được nhập thành công!');
-            }
-
-            return back()->with('error', 'File không chứa dữ liệu hoặc dữ liệu không hợp lệ!');
-        } catch (\Exception $e) {
-            return back()->with('error', 'Có lỗi xảy ra: ' . $e->getMessage());
-        }
-    }
-    public function updateQuestion(Request $request)
-    {
-        if (Auth::check()) {
-            try {
-                $checkQuestion = Quiz::where('question', $request->question_name);
-                if($checkQuestion) {
-                    return redirect()->route('quiz-bank.index')->with('error', 'Câu hỏi đã tồn tại!');
-                }
-                $id = $request->quiz_package_id;
-                $question = Quiz::with('quizPackage', 'choices')->findOrFail($id);
-                $isUpdated = false;
-
-                if ($request->question_name != $question->question) {
-                    $question->question = $request->question_name;
-                    $isUpdated = true;
-                }
-
-                foreach ($question->choices as $index => $choice) {
-                    $index++;  
-                    $anwserName = 'anwser_name_' . $index;
-
-                    if ($request->has($anwserName) && $request->$anwserName != $choice->choice) {
-                        $choice->choice = $request->$anwserName;
-                        $isUpdated = true;
-                    }
-
-                    if (isset($request->anwser)) {
-                        $correctAnwser = $index == $request->anwser ? 1 : 0;
-                        if ($choice->is_correct !== $correctAnwser) {
-                            $choice->is_correct = $correctAnwser;
-                            $isUpdated = true;
-                        }
-                    }
-                }
-
-                if ($isUpdated) {
-                    $question->save();
-                    foreach ($question->choices as $choice) {
-                        $choice->save();
-                    }
-                    return redirect()->route('quiz-bank.index')->with('success', 'Cập nhật câu hỏi thành công!');
-                }
-
-                return redirect()->route('quiz-bank.index')->with('error', 'Không có thay đổi nào để cập nhật!');
-            } catch (\Throwable $th) {
-                return back()->with('error', 'Có lỗi xảy ra trong quá trình cập nhật. Vui lòng thử lại.');
-            }
-        }
-
-        return redirect()->route('login')->with('error', 'Bạn cần đăng nhập để thực hiện hành động này');
-    }
-
-
-    public function deleteQuestion(Request $request)
-    {
-        if (Auth::check()) {
-            $id = $request->question_id;
-            // dd($id);
-            $asmID = AssignmentQuiz::where('quiz_id', $id)->first();
-            if ($asmID) {
-                return redirect()->route('quiz-bank.index')->with('error', 'Hiện tại không thể xóa câu hỏi này! Vui lòng kiểm tra lại Bài tập.');
-            }
-            $deleteQuestion = Quiz::findOrFail($id);
-            if ($deleteQuestion) {
-                $deleteQuestion->delete();
-                $choices = Choice::where('quiz_id', $deleteQuestion->id)->get();
-                if ($choices) {
-                    foreach ($choices as $choice) {
-                        $choice->delete();
-                    }
-                    return redirect()->route('quiz-bank.index')->with('success', 'Xóa câu hỏi thành công!');
-                }
-            }
-            return redirect()->route('quiz-bank.index')->with('error', 'Xóa câu hỏi thất bại!');
-        }
-        return redirect()->route('login')->with('error', 'Bạn cần đăng nhập để thực hiện hành động này');
     }
 }
