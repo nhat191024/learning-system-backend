7241d589c376a54791e682f3dfeea89bfc798f97
nhat191024 <richberchannel01@gmail.com>
2025-04-19 08:41:26 +0700

    feat: enhance store method to include title, description, duration, and date fields for class assignments

--- app/Http/Controllers/ClassAssignmentController.php
+++ app/Http/Controllers/ClassAssignmentController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Classes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Http\Requests\StoreAssignmentRequest;
 
 use App\Models\ClassAssignment;
 use App\Models\ClassSubmit;
+use App\Models\QuizPackage;
+use App\Models\AssignmentQuiz;
+
 
 class ClassAssignmentController extends Controller
 {
@@ -35,6 +41,47 @@ class ClassAssignmentController extends Controller
 
             return view('admin.class.assignment_detail', compact('classId', 'questions'));
         } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Error: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Store a new class assignment.
+     */
+    public function store(StoreAssignmentRequest $request, $classId)
+    {
+        DB::beginTransaction();
+        try {
+            $assignment = ClassAssignment::create([
+                'class_id' => $classId,
+                'type' => $request->type,
+                'title' => $request->title,
+                'description' => $request->description,
+                'duration' => $request->duration,
+                'start_date' => $request->start_date . ' ' . $request->start_time . ':00',
+                'due_date' => $request->due_date . ' ' . $request->due_time . ':00',
+                'status' => 'published',
+            ]);
+
+            if ($request->type === 'lab') {
+                DB::commit();
+                return redirect()->back()->with('success', 'Assignment created successfully!');
+            }
+
+            $package = QuizPackage::find($request->quiz_package_id);
+            $numberOfQuestions = $request->question_count;
+            for ($i = 0; $i < $numberOfQuestions; $i++) {
+                $quiz = $package->quizzes->random();
+                AssignmentQuiz::create([
+                    'assignment_id' => $assignment->id,
+                    'quiz_id' => $quiz->id,
+                ]);
+            }
+
+            DB::commit();
+            return redirect()->back()->with('success', 'Assignment created successfully!');
+        } catch (\Exception $e) {
+            DB::rollBack();
             return redirect()->back()->with('error', 'Error: ' . $e->getMessage());
         }
     }
